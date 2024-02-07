09f90cd665734603a8d63c10c19a2792d8bc724d
Dora343 <wusiukwan31623159@gmail.com>
2024-02-07 22:47:05 +0800

    Refactoring & add bot support

--- cmds/minigame.py
+++ cmds/minigame.py
@@ -1,75 +1,44 @@
-import discord
+
 from discord.ext import commands
-from core.classes import Cog_Extension
-import json
-import random
-import os
+from cmds.minigame_backend import MinigameBackend
 import typing as t
+import re
+class Minigame(MinigameBackend):
+    # file_path = os.path.dirname(__file__)
 
-class Minigame(Cog_Extension):
-    counter = 0
-    num = 194052
-    playercount = 0
-    # @commands.command()
-    # async def illuminati(self, ctx):
-    #     pic = discord.File(jdata['illuminati_pic'])
-    #     await ctx.send(file=pic)
-
-    file_path = os.path.dirname(__file__)
-
-    OK_STATUS = 200
-    GAMBLE_SUCCESS = 201
-    GAMBLE_FAIL = 202
-    # error status code:
-    # 401: invalid JSON
-    # 402: player profile not found
-    # 403: invalid value
-    # 404: players object not found
-    # 405: player profile already exist
-    # 406: bet is zero or negative
-    # 407: insufficient token
-    # 408: resurrect with non-zero token
-    # 499: Unexpected error
-    __JSON_ERROR = 401
-    __PLAYER_NOT_FOUND_ERROR = 402
-    __VALUE_ERROR = 403
-    __PLAYERS_DICT_NOT_FOUND_ERROR = 404
-    __PLAYER_ALREADY_EXIST_ERROR = 405
-    __NON_POSITIVE_BET_ERROR = 406
-    __INSUFFICIENT_TOKEN_ERROR = 407
-    __RESURRECT_WITH_TOKEN_ERROR = 408
-    __UNEXPECTED_ERROR = 499
+    def __init__(self, bot):
+        MinigameBackend.__init__(self, bot)
 
     async def handleError(self, ctx, status):
-        if (status == self.__PLAYER_NOT_FOUND_ERROR):
+        if (status == self._PLAYER_NOT_FOUND_ERROR):
             await ctx.message.reply('You are not registered.')
             return status
 
-        if (status == self.__VALUE_ERROR):
+        if (status == self._VALUE_ERROR):
             await ctx.message.reply('Invalid amount')
             return
 
-        if (status == self.__PLAYERS_DICT_NOT_FOUND_ERROR):
+        if (status == self._PLAYERS_DICT_NOT_FOUND_ERROR):
             await ctx.message.reply('missing player dict')
             return status
 
-        if (status == self.__PLAYER_ALREADY_EXIST_ERROR):
+        if (status == self._PLAYER_ALREADY_EXIST_ERROR):
             await ctx.message.reply('You are already registered')
             return status
         
-        if (status == self.__NON_POSITIVE_BET_ERROR):
+        if (status == self._NON_POSITIVE_BET_ERROR):
             await ctx.message.reply('You cannot place zero/negative bet.')
             return status
 
-        if (status == self.__INSUFFICIENT_TOKEN_ERROR):
+        if (status == self._INSUFFICIENT_TOKEN_ERROR):
             await ctx.message.reply('You do not have enough tokens.')
             return status
 
-        if (status == self.__INSUFFICIENT_TOKEN_ERROR):
+        if (status == self._INSUFFICIENT_TOKEN_ERROR):
             await ctx.message.reply('You can only resurrect with 0 token.')
             return status
 
-        if (status == self.__UNEXPECTED_ERROR):
+        if (status == self._UNEXPECTED_ERROR):
             await ctx.message.reply('something broke unexpectedly')    
             return status
 
@@ -77,169 +46,7 @@ class Minigame(Cog_Extension):
 
 
 
-    def readData(self):
-        data = dict()
-
-        with open(self.file_path + '/minigame-data.json','r', encoding='utf-8') as jsonfile:
-            try:
-                data = json.load(jsonfile)
-            except json.decoder.JSONDecodeError:
-                return self.__JSON_ERROR
-
-        return data
-
-    def writeData(self, data):
-        with open(self.file_path + '/minigame-data.json','w', encoding='utf-8') as jsonfile:
-                try:
-                    json.dump(data, jsonfile, indent=2)
-                except json.decoder.JSONDecodeError:
-                    return self.__JSON_ERROR
-        
-        return self.OK_STATUS
-
-    def isValidPlayer(self, playerid):
-        data = self.readData()
-        if (data.get('players', -1) == -1): return self.__PLAYERS_DICT_NOT_FOUND_ERROR
-        if (data.get('players').get(str(playerid), -1) == -1): return self.__PLAYER_NOT_FOUND_ERROR
-
-        return self.OK_STATUS
-
-    def createAccount(self, targetid, name):
-
-        data = self.readData()
-        if (data.get('players', -1) == -1):
-            # create players dict
-            data['players'] = dict()
-
-        if (data.get('players').get(str(targetid), -1) != -1): return self.__PLAYER_ALREADY_EXIST_ERROR
-            
-        template_player_profile = {
-            'name': name, 
-            'status': 'active',
-            'currency': 100,
-            'resurrection-count': 0 
-        }
-
-        data['players'][str(targetid)] = template_player_profile
-        self.playercount = data['player-count']
-        self.playercount = self.playercount + 1
-        data['player-count'] = self.playercount
-
-        self.writeData(data)
-        return self.OK_STATUS
-
-    def deleteAccount(self, targetid):
-
-        status = self.isValidPlayer(targetid)
-        if (status != self.OK_STATUS):
-            return status
-
-
-        data = self.readData()
-        if (data['players'].pop(str(targetid), 499) == 499):
-            return self.__UNEXPECTED_ERROR
-        self.playercount = data['player-count']
-        self.playercount = self.playercount - 1
-        data['player-count'] = self.playercount
-
-        self.writeData(data)
-        return self.OK_STATUS
-
-    def getToken(self, targetid):
-
-        if (self.isValidPlayer(str(targetid)) != self.OK_STATUS):
-            return self.__PLAYER_NOT_FOUND_ERROR, 0
-        
-        data = self.readData()
-        return self.OK_STATUS, data.get('players').get(str(targetid)).get('currency')
-
-    def addToken(self, amount, targetid):
-        flag = True
-        try:
-            int(amount)
-        except ValueError:
-            flag = False
-        currency = 0
-        if (not(flag)): 
-            return self.__VALUE_ERROR
-
-
-        data = self.readData()
-
-        if (self.isValidPlayer(str(targetid)) != self.OK_STATUS):
-            return self.__PLAYER_NOT_FOUND_ERROR
-
-        currency = data['players'][str(targetid)]['currency']
-        currency = currency + int(amount)
-        data['players'][str(targetid)]['currency'] = currency
-        if (data.get('players').get(str(targetid), -1) == -1):
-            # create highest-token
-            data['players'][str(targetid)]['highest-token'] = dict()
-        self.writeData(data)
-        return self.OK_STATUS
-
-    def gamble(self, amount, targetid):
-        data = self.readData()
-        currency = data.get('players', {}).get(str(targetid), {}).get('currency', -1)
-        
-        if (currency == -1):
-            return self.__PLAYER_NOT_FOUND_ERROR, 0
-
-        amount = int(amount)
-        if (amount <= 0):
-            return self.__NON_POSITIVE_BET_ERROR, 0
-
-        if (amount > int(currency)):
-            return self.__INSUFFICIENT_TOKEN_ERROR, 0
-
-        odds = 0.5 * 1000
-
-        temp = random.randrange(1, 1000)
-        
-        if (temp > odds):
-            amount = -1*int(amount) 
-        
-
-        status = self.addToken(str(-1*amount), targetid)
-        if status == 1: 
-            return self.__VALUE_ERROR, 0
-
-        if status == 2:
-            return self.__PLAYER_NOT_FOUND_ERROR, 0
-
-        if (temp > odds): return self.OK_STATUS, self.GAMBLE_SUCCESS
-        if (temp <= odds): return self.OK_STATUS, self.GAMBLE_FAIL
-
-    def resurrect(self, targetid):
-        if (self.isValidPlayer(str(targetid)) != self.OK_STATUS):
-            return self.__PLAYER_NOT_FOUND_ERROR
-            
-        status = self.getToken(targetid)
-
-        if (status[1] != 0):
-            return self.__RESURRECT_WITH_TOKEN_ERROR, 0
-
-        data = self.readData()
-        resurrect_token = data['resurrect-token']
-        status = self.addToken(resurrect_token, targetid)
-        
-        if (status != self.OK_STATUS): return
-
-
-        data = self.readData()
-
-        # create entry if resurrection-count entry does not exist
-        if data.get('players').get(str(targetid)).get('resurrection-count', -1) == -1:
-            data['players'][str(targetid)]['resurrection-count'] = 0
-
-        data['players'][str(targetid)]['resurrection-count'] = data['players'][str(targetid)]['resurrection-count'] + 1
-        self.writeData(data)
-
-        return self.OK_STATUS, resurrect_token
-
-    def transferToken(self, srcid, dstid, amount):
-        # data = await self.readData(ctx)
-        pass
+    
 
 
     @commands.command(name='createAccount',aliases=['register'])
@@ -247,7 +54,7 @@ class Minigame(Cog_Extension):
         
         status = self.createAccount(ctx.author.id, ctx.author.name)
         await self.handleError(ctx, status)
-        if (status != self.OK_STATUS): return
+        if (status != self._OK_STATUS): return
         
         await ctx.message.reply('player profile created.')
 
@@ -256,7 +63,7 @@ class Minigame(Cog_Extension):
         
         status = self.deleteAccount(ctx.author.id)
         await self.handleError(ctx, status)
-        if (status != self.OK_STATUS): return
+        if (status != self._OK_STATUS): return
 
         await ctx.message.reply('Your account is deleted.')
             
@@ -268,13 +75,13 @@ class Minigame(Cog_Extension):
 
         status = self.getToken(targetid)
         await self.handleError(ctx, status[0])
-        if (status[0] != self.OK_STATUS): return
+        if (status[0] != self._OK_STATUS): return
   
         await ctx.message.reply(F'You have {status[1]} tokens.')
         if (int(status[1]) == 0):
             await ctx.send('Use .revive to start again.')
 
-    @commands.command(name='addCurrency',aliases=['addtoken'])
+    @commands.command(name='addToken',aliases=['addtoken'])
     async def addTokenCommand(self, ctx, amount, targetid:t.Optional[str]):
         if (ctx.author.id != 400941378395439104):
             await ctx.message.reply('You do not have permission to do so.')
@@ -285,7 +92,7 @@ class Minigame(Cog_Extension):
 
         status = self.addToken(amount, targetid)
         await self.handleError(ctx, status)
-        if (status != self.OK_STATUS): return
+        if (status != self._OK_STATUS): return
 
         await ctx.message.reply(F'Added {amount} tokens.')
         
@@ -297,16 +104,16 @@ class Minigame(Cog_Extension):
         
         status = self.gamble(amount, ctx.author.id)
         await self.handleError(ctx, status[0])
-        if (status[0] != self.OK_STATUS): return
+        if (status[0] != self._OK_STATUS): return
 
         data = self.readData()
         currency = data.get('players', {}).get(str(ctx.author.id), {}).get('currency', -1)
 
-        if (status[1] == self.GAMBLE_SUCCESS):
+        if (status[1] == self._GAMBLE_SUCCESS):
             await ctx.message.reply(F'Success! **{amount} tokens** have been added to your account.\nYou have {currency} tokens.')
             return 0
 
-        if (status[1] == self.GAMBLE_FAIL):
+        if (status[1] == self._GAMBLE_FAIL):
             await ctx.message.reply(F'Unlucky! **{amount} tokens** have been taken from your account.\nYou have {currency} tokens.')
             if (int(currency) == 0):
                 await ctx.send('Use .revive to start again.')
@@ -325,10 +132,85 @@ class Minigame(Cog_Extension):
     # async def getLoginBonus(self, ctx):
 
     
+class BotMinigame(Minigame):
+
+    def __init__(self, bot):
+        MinigameBackend.__init__(self, bot)
+        Minigame.__init__(self, bot)
 
 
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+
+
+        # reject normie
+        if msg.author.bot != True:
+            return
+
+        # reject this bot
+        if msg.author.id == self.bot.user.id:
+            # await msg.channel.send('yay')
+            return    
+
+        ctx = await self.bot.get_context(msg)
+
+        patternCreateAccount    = "^[.](createAccount|register)$"
+        patternDeleteAccount    = "^[.](deleteAccount|quitgame)$"
+        patternGetToken         = "^[.](getToken|token)$"
+        patternGetTokenOpt      = "^[.](getToken|token)[ ][0-9]{1,}$"
+        patternAddToken         = "^[.](addToken|addtoken)[ ](-[1-9][0-9]{0,}|[1-9][0-9]{0,})$"
+        patternAddTokenOpt      = "^[.](addToken|addtoken)[ ](-[1-9][0-9]{0,}|[1-9][0-9]{0,})[ ][0-9]{1,}$"
+        patternGamble           = "^[.](gamble|g)$"
+        patternGambleOpt        = "^[.](gamble|g)[ ][0-9]{1,}$"
+        patternResurrect        = "^[.](resurrect|revive|復活)$"
+
+        if (re.match(patternCreateAccount,msg.content)):
+            await self.createAccountCommand(ctx)
+            return
+
+        if (re.match(patternDeleteAccount,msg.content)):
+            await self.deleteAccountCommand(ctx)
+            return    
+
+        if (re.match(patternGetToken,msg.content)):
+            await self.getTokenCommand(ctx,None)
+            return
+
+        if (re.match(patternGetTokenOpt,msg.content)):
+            arg1 = re.findall("[0-9]{1,}",msg.content)[0] 
+            await self.getTokenCommand(ctx,arg1)
+            return
+
+                
+
+        if (re.match(patternAddToken,msg.content)):
+            arg1 = re.findall("(-[1-9][0-9]{0,}|[1-9][0-9]{0,})",msg.content)[0]
+            await self.addTokenCommand(ctx,arg1,None)
+            return
+
+        if (re.match(patternAddTokenOpt,msg.content)):
+            arg1 = re.findall("(-[1-9][0-9]{0,}|[1-9][0-9]{0,})",msg.content)[0]
+            arg2 = re.findall("[0-9]{1,}",msg.content)[0]
+            await self.addTokenCommand(ctx,arg1,arg2)
+            return
+
+        if (re.match(patternGamble,msg.content)):
+            await self.gambleCommand(ctx,None)
+            return  
+
+        if (re.match(patternGambleOpt,msg.content)):
+            arg1 = re.findall("[0-9]{1,}",msg.content)[0]
+            await self.gambleCommand(ctx,arg1)
+            return            
+
+        if (re.match(patternResurrect,msg.content)):
+            await self.resurrectCommand(ctx)
+            return
+ 
 
 
 
 def setup(bot):
-    bot.add_cog(Minigame(bot))
+    # bot.add_cog(Minigame(bot))
+    bot.add_cog(BotMinigame(bot))
---
+++ cmds/minigame_backend.py
@@ -0,0 +1,208 @@
+
+from core.classes import Cog_Extension
+import json
+import random
+import os
+
+class MinigameBackend(Cog_Extension):
+
+    def __init__(self, bot):
+        super().__init__(bot)
+
+
+        self._file_path = os.path.dirname(__file__)
+
+        self._OK_STATUS = 200
+        self._GAMBLE_SUCCESS = 201
+        self._GAMBLE_FAIL = 202
+        # error status code:
+        # 401: invalid JSON
+        # 402: player profile not found
+        # 403: invalid value
+        # 404: players object not found
+        # 405: player profile already exist
+        # 406: bet is zero or negative
+        # 407: insufficient token
+        # 408: resurrect with non-zero token
+        # 499: Unexpected error
+        self._JSON_ERROR = 401
+        self._PLAYER_NOT_FOUND_ERROR = 402
+        self._VALUE_ERROR = 403
+        self._PLAYERS_DICT_NOT_FOUND_ERROR = 404
+        self._PLAYER_ALREADY_EXIST_ERROR = 405
+        self._NON_POSITIVE_BET_ERROR = 406
+        self._INSUFFICIENT_TOKEN_ERROR = 407
+        self._RESURRECT_WITH_TOKEN_ERROR = 408
+        self._UNEXPECTED_ERROR = 499
+    
+    def readData(self):
+        data = dict()
+
+        with open(self._file_path + '/minigame-data.json','r', encoding='utf-8') as jsonfile:
+            try:
+                data = json.load(jsonfile)
+            except json.decoder.JSONDecodeError:
+                return self._JSON_ERROR
+
+        return data
+
+    def writeData(self, data):
+        with open(self._file_path + '/minigame-data.json','w', encoding='utf-8') as jsonfile:
+                try:
+                    json.dump(data, jsonfile, indent=2)
+                except json.decoder.JSONDecodeError:
+                    return self._JSON_ERROR
+        
+        return self._OK_STATUS
+
+    def isValidPlayer(self, playerid):
+        data = self.readData()
+        if (data.get('players', -1) == -1): return self._PLAYERS_DICT_NOT_FOUND_ERROR
+        if (data.get('players').get(str(playerid), -1) == -1): return self._PLAYER_NOT_FOUND_ERROR
+
+        return self._OK_STATUS
+
+    def createAccount(self, targetid, name):
+
+        data = self.readData()
+        if (data.get('players', -1) == -1):
+            # create players dict
+            data['players'] = dict()
+
+        if (data.get('players').get(str(targetid), -1) != -1): return self._PLAYER_ALREADY_EXIST_ERROR
+            
+        template_player_profile = {
+            'name': name, 
+            'status': 'active',
+            'currency': 100,
+            'resurrection-count': 0,
+            'highest-token': 100
+        }
+
+        data['players'][str(targetid)] = template_player_profile
+        self.playercount = data['player-count']
+        self.playercount = self.playercount + 1
+        data['player-count'] = self.playercount
+
+        self.writeData(data)
+        return self._OK_STATUS
+
+    def deleteAccount(self, targetid):
+
+        status = self.isValidPlayer(targetid)
+        if (status != self._OK_STATUS):
+            return status
+
+
+        data = self.readData()
+        if (data['players'].pop(str(targetid), 499) == 499):
+            return self._UNEXPECTED_ERROR
+        self.playercount = data['player-count']
+        self.playercount = self.playercount - 1
+        data['player-count'] = self.playercount
+
+        self.writeData(data)
+        return self._OK_STATUS
+
+    def getToken(self, targetid):
+
+        if (self.isValidPlayer(str(targetid)) != self._OK_STATUS):
+            return self._PLAYER_NOT_FOUND_ERROR, 0
+        
+        data = self.readData()
+        return self._OK_STATUS, data.get('players').get(str(targetid)).get('currency')
+
+    def addToken(self, amount, targetid):
+        flag = True
+        try:
+            int(amount)
+        except ValueError:
+            flag = False
+        currency = 0
+        if (not(flag)): 
+            return self._VALUE_ERROR
+
+
+        data = self.readData()
+
+        if (self.isValidPlayer(str(targetid)) != self._OK_STATUS):
+            return self._PLAYER_NOT_FOUND_ERROR
+
+        currency = data['players'][str(targetid)]['currency']
+        currency = currency + int(amount)
+        data['players'][str(targetid)]['currency'] = currency
+        if (data.get('players').get(str(targetid)).get('highest-token', -1) == -1):
+            # create highest-token
+            data['players'][str(targetid)]['highest-token'] = 0
+
+        highest = data['players'][str(targetid)]['highest-token']
+        if (currency>highest):
+            data['players'][str(targetid)]['highest-token'] = currency
+        self.writeData(data)
+        return self._OK_STATUS
+
+    def gamble(self, amount, targetid):
+        data = self.readData()
+        currency = data.get('players', {}).get(str(targetid), {}).get('currency', -1)
+        
+        if (currency == -1):
+            return self._PLAYER_NOT_FOUND_ERROR, 0
+
+        amount = int(amount)
+        if (amount <= 0):
+            return self._NON_POSITIVE_BET_ERROR, 0
+
+        if (amount > int(currency)):
+            return self._INSUFFICIENT_TOKEN_ERROR, 0
+
+        odds = 0.5 * 1000
+
+        temp = random.randrange(1, 1000)
+        
+        if (temp > odds):
+            amount = -1*int(amount) 
+        
+
+        status = self.addToken(str(-1*amount), targetid)
+        if status == 1: 
+            return self._VALUE_ERROR, 0
+
+        if status == 2:
+            return self._PLAYER_NOT_FOUND_ERROR, 0
+
+        if (temp > odds): return self._OK_STATUS, self._GAMBLE_SUCCESS
+        if (temp <= odds): return self._OK_STATUS, self._GAMBLE_FAIL
+
+    def resurrect(self, targetid):
+        if (self.isValidPlayer(str(targetid)) != self._OK_STATUS):
+            return self._PLAYER_NOT_FOUND_ERROR
+            
+        status = self.getToken(targetid)
+
+        if (status[1] != 0):
+            return self._RESURRECT_WITH_TOKEN_ERROR, 0
+
+        data = self.readData()
+        resurrect_token = data['resurrect-token']
+        status = self.addToken(resurrect_token, targetid)
+        
+        if (status != self._OK_STATUS): return
+
+
+        data = self.readData()
+
+        # create entry if resurrection-count entry does not exist
+        if data.get('players').get(str(targetid)).get('resurrection-count', -1) == -1:
+            data['players'][str(targetid)]['resurrection-count'] = 0
+
+        data['players'][str(targetid)]['resurrection-count'] = data['players'][str(targetid)]['resurrection-count'] + 1
+        self.writeData(data)
+
+        return self._OK_STATUS, resurrect_token
+
+    def transferToken(self, srcid, dstid, amount):
+        # data = await self.readData(ctx)
+        pass
+
+def setup(bot):
+    pass
