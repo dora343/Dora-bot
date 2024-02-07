a225c4b1a942d6fde964ea8930d081fe9b7a5b44
Dora343 <wusiukwan31623159@gmail.com>
2024-02-07 18:01:37 +0800

    Fix an oversight in venv

--- start_bot.sh
+++ start_bot.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 git update-index --skip-worktree settings.json
 git update-index --skip-worktree cmds/minigame-data.json
-# python3 ensurepip --upgrade
 python3 -m venv venv
 source venv/bin/activate
-
 pip install pip --upgrade
 pip install -r requirements.txt
 python3 bot.py
-source venv/bin/deactivate
