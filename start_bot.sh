c1e4283325c4170ca3e7359ab85e709994a4cd5f
Dora343 <wusiukwan31623159@gmail.com>
2024-02-07 17:58:06 +0800

    venv implementation

--- start_bot.sh
+++ start_bot.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 git update-index --skip-worktree settings.json
 git update-index --skip-worktree cmds/minigame-data.json
-python3 ensurepip --upgrade
+# python3 ensurepip --upgrade
+python3 -m venv venv
+source venv/bin/activate
+
 pip install pip --upgrade
 pip install -r requirements.txt
 python3 bot.py
+source venv/bin/deactivate
